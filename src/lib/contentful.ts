a11976ab89d47bfc923005f7eea8404427483465
Brian Garland <Brian.Garland@simplot.com>
2023-01-03 14:36:31 -0700

    config

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -57,17 +57,17 @@ export interface AstroInstance {
     default: AstroComponent;
   }
 
-// export const contentfulClient = contentful.createClient({
-//   space: import.meta.env.CONTENTFUL_SPACE_ID,
-//   accessToken: import.meta.env.DEV
-//     ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-//     : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-//   host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
-// });
-
 export const contentfulClient = contentful.createClient({
-  space: "803kc1al70mk",
-  accessToken: "CFPAT-cIVYxoYSm6cbdqudRRjD-toCWMk5vSwXdfWimjgEmlU",
-  host: "cdn.contentful.com",
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
+
+// export const contentfulClient = contentful.createClient({
+//   space: "803kc1al70mk",
+//   accessToken: "CFPAT-cIVYxoYSm6cbdqudRRjD-toCWMk5vSwXdfWimjgEmlU",
+//   host: "cdn.contentful.com",
+// });
 
